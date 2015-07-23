200e9ecb683cae4fce70d8d61d0958d4e8c5bb2f
刘浩浩 <Codeliu@Code-LiudeMacBook-Pro.local>
2015-07-23 17:17:33 +0800

    Initial Commit

---
+++ 字体间距/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  字体间距
+//
+//  Created by 刘浩浩 on 15/7/23.
+//  Copyright (c) 2015年 Codeliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
